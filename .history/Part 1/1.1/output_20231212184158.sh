d7db927a48b83b12acf798ae5c84d0e7c626bfdc
The Humanist <42555733+TheHumanistX@users.noreply.github.com>
2023-12-12 18:42:47 -0500

    Finished Exercise 1.1

---
+++ .history/Part 1/1.1/output_20231212184158.sh
@@ -0,0 +1,6 @@
+Brian H@DESKTOP-3IU4USR MINGW64 ~/Coding/mooc-helsinki/devops (test)
+$ docker ps -a
+CONTAINER ID   IMAGE     COMMAND                  CREATED          STATUS                      PORTS     NAMES
+c449db2b2706   nginx     "/docker-entrypoint.…"   52 seconds ago   Exited (0) 28 seconds ago             vigorous_saha
+a12ff75c52a5   nginx     "/docker-entrypoint.…"   55 seconds ago   Exited (0) 28 seconds ago             festive_blackwell
+74e11d4a09c3   nginx     "/docker-entrypoint.…"   2 minutes ago    Up 2 minutes                80/tcp    compassionate_margulis
